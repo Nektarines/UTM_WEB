08d42a4462ddfa1bd1658e298bd49c6da4ab7f5f
Nick <grozav.nikita@gmail.com>
2024-04-12 16:03:42 +0300

    working components with main page and login

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import './styles/index.css';
+import './styles/root.css';
+import AppRouter from './components/AppRouter';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<AppRouter />
+);
